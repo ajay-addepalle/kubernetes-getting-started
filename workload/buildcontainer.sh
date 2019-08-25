b41ff08cd2fa1671a8c11741db89bc178b3ea522
Ajay Addepalle <32472214+ajay-addepalle@users.noreply.github.com>
2019-08-26 00:20:37 +0530

    added deployments

--- workload/buildcontainer.sh
+++ workload/buildcontainer.sh
@@ -1,2 +1,2 @@
 eval $(minikube docker-env)
-docker build -t node-kube .
+docker build -t node-kube:v1 .
--- workload/server.js
+++ workload/server.js
@@ -9,7 +9,7 @@ const handleRequest = function(request, response) {
     counter++;
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end(`\nHello World!\nCalled ${counter} times!\n${uuid}\nMY_NODE_NAME:${process.env.MY_NODE_NAME}\nMY_POD_NAME:${process.env.MY_POD_NAME}\nMY_POD_NAMESPACE:${process.env.MY_POD_NAMESPACE}\nMY_POD_IP:${process.env.MY_POD_IP}\nMY_POD_SERVICE_ACCOUNT:${process.env.MY_POD_SERVICE_ACCOUNT}\n`);
+  response.end(`\nHello WorldV1!\nCalled ${counter} times!\n${uuid}\nMY_NODE_NAME:${process.env.MY_NODE_NAME}\nMY_POD_NAME:${process.env.MY_POD_NAME}\nMY_POD_NAMESPACE:${process.env.MY_POD_NAMESPACE}\nMY_POD_IP:${process.env.MY_POD_IP}\nMY_POD_SERVICE_ACCOUNT:${process.env.MY_POD_SERVICE_ACCOUNT}\n`);
 };
 const www = http.createServer(handleRequest);
 www.listen(4080);
