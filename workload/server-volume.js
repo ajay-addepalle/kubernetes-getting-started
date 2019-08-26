c60a7575665290d0f5b3c499933358e09fb52991
Ajay Addepalle <32472214+ajay-addepalle@users.noreply.github.com>
2019-08-26 09:17:36 +0530

    finished volumes

--- workload/server-volume.js
+++ workload/server-volume.js
@@ -6,26 +6,30 @@ const fs = require('fs');
 const path = require('path');
 
 const VOLUME_PATH = process.env.VOLUME_PATH;
-    
+
 let counter = 0;
 const uuid = uuidv4();
+const dataFilePath = path.join(VOLUME_PATH, 'data.txt');
+
 const handleRequest = function(request, response) {
     counter++;
   console.log('Received request for URL: ' + request.url);    
   response.writeHead(200);
-  if(VOLUME_PATH == undefined || VOLUME_PATH == 'undefined') {
-    fs.readFile(path.join(VOLUME_PATH, 'data.txt'), {encoding: 'utf-8'} , function(err, data){
+  if(VOLUME_PATH != undefined && VOLUME_PATH != 'undefined') {
+    fs.readFile(dataFilePath, function(err, data){
         if(!err){
             response.write(data);
+            response.write(`\nFilePath:${dataFilePath}`);
+            response.end(`\nHello WorldV1!\nCalled ${counter} times!\n${uuid}\n`);
         } else {
             response.write(`Error reading file: ${err}`);
+            response.end(`\nHello WorldV1!\nCalled ${counter} times!\n${uuid}\n`);
         }
     });
-    response.write('');
   } else {
     response.write('\nCannot read file, path not provided!');
+    response.end(`\nHello WorldV1!\nCalled ${counter} times!\n${uuid}\n`);
   }
-  response.end(`\nHello WorldV1!\nCalled ${counter} times!\n${uuid}\nMY_NODE_NAME:${process.env.MY_NODE_NAME}\nMY_POD_NAME:${process.env.MY_POD_NAME}\nMY_POD_NAMESPACE:${process.env.MY_POD_NAMESPACE}\nMY_POD_IP:${process.env.MY_POD_IP}\nMY_POD_SERVICE_ACCOUNT:${process.env.MY_POD_SERVICE_ACCOUNT}\n`);
 };
 const www = http.createServer(handleRequest);
 www.listen(4080);
